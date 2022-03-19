2e1a7a9af45e9e1e2c552a4be062cd528c8267b8
Denya1 <Denya6461@gmail.com>
2022-03-19 18:29:14 +0200

    StartTestApiTesting

---
+++ Api/PostRequestBody.cs
@@ -0,0 +1,146 @@
+﻿namespace RequestBody
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class PostRequestBody
+    {
+
+
+        public partial class Welcome
+        {
+            [JsonProperty("formProperties")]
+            public FormProperties FormProperties { get; set; }
+
+            [JsonProperty("emailConfig")]
+            public EmailConfig EmailConfig { get; set; }
+
+            [JsonProperty("viewMode")]
+            public string ViewMode { get; set; }
+
+            [JsonProperty("fields")]
+            public Field[] Fields { get; set; }
+
+            [JsonProperty("labelIds")]
+            public string[] LabelIds { get; set; }
+        }
+
+        public partial class EmailConfig
+        {
+            [JsonProperty("sendToOwnerAndEmails")]
+            public SendToOwnerAndEmails SendToOwnerAndEmails { get; set; }
+        }
+
+        public partial class SendToOwnerAndEmails
+        {
+            [JsonProperty("emailIds")]
+            public object[] EmailIds { get; set; }
+        }
+
+        public partial class Field
+        {
+            [JsonProperty("fieldId")]
+            public string FieldId { get; set; }
+
+            [JsonProperty("label")]
+            public string Label { get; set; }
+
+            [JsonProperty("firstName", NullValueHandling = NullValueHandling.Ignore)]
+            public FirstName FirstName { get; set; }
+
+            [JsonProperty("email", NullValueHandling = NullValueHandling.Ignore)]
+            public Email Email { get; set; }
+        }
+
+        public partial class Email
+        {
+            [JsonProperty("value")]
+            public string Value { get; set; }
+
+            [JsonProperty("tag")]
+            public string Tag { get; set; }
+        }
+
+        public partial class FirstName
+        {
+            [JsonProperty("value")]
+            public string Value { get; set; }
+        }
+
+        public partial class FormProperties
+        {
+            [JsonProperty("formName")]
+            public string FormName { get; set; }
+
+            [JsonProperty("formId")]
+            public string FormId { get; set; }
+        }
+    }
+
+
+    //    [JsonProperty("formProperties")]
+    //    public FormProperties FormProperties { get; set; }
+
+    //    [JsonProperty("emailConfig")]
+    //    public EmailConfig EmailConfig { get; set; }
+
+    //    [JsonProperty("fields")]
+    //    public Field[] Fields { get; set; }
+    //}
+
+    //public partial class EmailConfig
+    //{
+    //    [JsonProperty("sendToOwnerAndEmails")]
+    //    public SendToOwnerAndEmails SendToOwnerAndEmails { get; set; }
+    //}
+
+    //public partial class SendToOwnerAndEmails
+    //{
+    //    [JsonProperty("emailIds")]
+    //    public object[] EmailIds { get; set; }
+    //}
+
+    //public partial class Field
+    //{
+    //    [JsonProperty("fieldId")]
+    //    public string FieldId { get; set; }
+
+    //    [JsonProperty("label")]
+    //    public string Label { get; set; }
+
+    //    [JsonProperty("firstName", NullValueHandling = NullValueHandling.Ignore)]
+    //    public FirstName FirstName { get; set; }
+
+    //    [JsonProperty("email", NullValueHandling = NullValueHandling.Ignore)]
+    //    public Email Email { get; set; }
+    //}
+
+    //public partial class Email
+    //{
+    //    [JsonProperty("value")]
+    //    public string Value { get; set; }
+
+    //    [JsonProperty("tag")]
+    //    public string Tag { get; set; }
+    //}
+
+    //public partial class FirstName
+    //{
+    //    [JsonProperty("value")]
+    //    public string Value { get; set; }
+    //}
+
+    //public partial class FormProperties
+    //{
+    //    [JsonProperty("formName")]
+    //    public string FormName { get; set; }
+
+    //    [JsonProperty("formId")]
+    //    public string FormId { get; set; }
+    //}
+}
+
---
+++ Tests/ApiTests.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using RequestBody;
+using RestSharp;
+using System.Net;
+using System.Threading;
+using static RequestBody.PostRequestBody;
+
+namespace SeleniumTest.Tests
+{
+    [TestFixture]
+    public class ApiTests
+    {
+        [Test]
+        public void VerifyPostRequestPassed()
+        {
+            var postRequestBody = new PostRequestBody()
+            {
+                Welcome = new Welcome()
+                {
+
+                
+                FormProperties = new FormProperties()
+                {
+                    FormId = "comp-jxd8gc7s",
+                    FormName = "Contacts Form 2",
+                },
+                EmailConfig = new EmailConfig()
+                {
+                    SendToOwnerAndEmails = new SendToOwnerAndEmails()
+                    {
+                        EmailIds = new object[] {}
+
+                    }
+                },
+                ViewMode = "Site",
+                    LabelIds = new string[]
+                    {
+                        "contacts-contacted_me",
+                        "0e84ae80-8bf1-43a6-ba05-8eefb8cbc86b",
+                        "2f48386d-f13f-455f-a00f-9d697ef76de6"
+                    }
+                },
+                
+                Fields = new Field[]
+               {
+                  new Field()
+                 {
+                     FieldId = "comp-jxd8gc8n",
+                     Label = "First Name",
+                     FirstName = new FirstName()
+                     {
+                         Value ="кеке"
+                     },
+
+                 },
+                   new Field()
+                   {
+                       FieldId = "comp-jxd8gc99" ,
+                       Label = "Email",
+                       Email = new Email()
+                       {
+                           Value = "Denya6461@gmail.com",
+                           Tag = "main",
+                       }
+                   },
+
+
+               },
+
+
+
+
+            };
+
+            IRestClient client = new RestClient("https://tarasmysko89.wixsite.com/_api/wix-forms/v1/");
+
+
+            RestRequest request = new RestRequest("submit-form", Method.POST);
+
+            postRequestBody.Fields[0].FieldId = "Test";
+
+            var test = JsonConvert.SerializeObject(postRequestBody);
+
+            request.AddJsonBody(test);
+
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authorization", "b8Ac4G_ssoIRdmu_45Raf11Mezo0HCwnZ091aHD88zU.eyJpbn" +
+                              "N0YW5jZUlkIjoiNGEwMDU1ZjItNDIyYy00NDkxLTgxMDMtYjFiMTMzYTc5MjkxIiwiY" +
+                              "XBwRGVmSWQiOiIxNGNlMTIxNC1iMjc4LWE3ZTQtMTM3My0wMGNlYmQxYmVmN2MiLCJtZ" +
+                              "XRhU2l0ZUlkIjoiYzFkNjdlOTAtZmE1YS00ZjZiLTkyM2EtYWM4NGQ2NjBkNThhIiwic2" +
+                              "lnbkRhdGUiOiIyMDIyLTAzLTE5VDExOjU0OjEyLjEyNVoiLCJkZW1vTW9kZSI6ZmFsc2Us" +
+                              "Im9yaWdpbkluc3RhbmNlSWQiOiI5NTU3MjYxYi01N2I0LTRlMjItOTNjNy02MGIzYjZiM2U" +
+                              "2MmUiLCJhaWQiOiJkN2ZhYTUxZS1jNWVlLTRkMjEtOGViOS02NDMxYWEwZGQ0YTEiLCJi" +
+                              "aVRva2VuIjoiOGJkNjJiNjItYjg3Ni0wYmZhLTEzMzktMWQzNWU1Yzc0NzFiIiwic2l0Z" +
+                              "U93bmVySWQiOiJjODNhMmJjMy05NjNlLTQzMmItOWM1MC1hOWM5YjhmMzFhMGQifQ");
+
+            IRestResponse response = client.Execute(request);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(response.Content.Contains("submissionId"));
+
+
+
+        }
+
+    }
+}
